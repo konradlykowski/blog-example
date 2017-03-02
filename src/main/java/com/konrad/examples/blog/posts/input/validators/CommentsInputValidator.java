aebe13d919e2221bb67d33932f4c76bb1481266f
Konrad Lykowski <konrad.lykowski@outlook.com>
2017-03-02 20:15:40 +0100

    add new func

--- src/main/java/com/konrad/examples/blog/posts/input/validators/CommentsInputValidator.java
+++ src/main/java/com/konrad/examples/blog/posts/input/validators/CommentsInputValidator.java
@@ -1,23 +1,31 @@
 package com.konrad.examples.blog.posts.input.validators;
 
+import com.konrad.examples.blog.comments.domain.Comment;
 import com.konrad.examples.blog.posts.input.validators.impl.InputFilter;
 import com.konrad.examples.blog.posts.input.validators.impl.JsonEscaperFilter;
 import com.konrad.examples.blog.posts.input.validators.impl.LengthValidator;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
 
 public class CommentsInputValidator {
 
     public Validator[] validators = new Validator[3];
 
     public CommentsInputValidator() {
-        validators[0] = new LengthValidator(1500);
-        validators[1] = new InputFilter();
-        validators[2] = new JsonEscaperFilter();
+
     }
 
-    public String validate(String input) {
-        for (Validator<String> validator : validators) {
-            input = validator.validate(input);
+    public Comment validate(Comment comment) {
+        Set<ConstraintViolation<Comment>> constraintViolations = Validation.buildDefaultValidatorFactory().getValidator().validate(comment);
+        if (constraintViolations.size() != 0) {
+            throw new IllegalArgumentException("Comment not valid.");
         }
-        return input;
+        DefaultInputValidator defaultInputValidator = new DefaultInputValidator();
+        comment.setName(defaultInputValidator.validate(comment.getName()));
+        comment.setText(defaultInputValidator.validate(comment.getText()));
+        return comment;
     }
 }
