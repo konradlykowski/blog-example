18a466f651e44e79eef808741dc12586ec740001
Konrad Lykowski <konrad.lykowski@outlook.com>
2017-04-21 12:21:26 +0200

    add new func

--- src/main/java/com/konrad/examples/blog/posts/filter/FilterFields.java
+++ src/main/java/com/konrad/examples/blog/posts/filter/FilterFields.java
@@ -1,7 +1,7 @@
 package com.konrad.examples.blog.posts.filter;
 
 public enum FilterFields {
-    CATEGORY("category"), LOCATION("location"), TAG("tag"), ALL("all");
+    category("category"), location("location"), tag("tag"), all("all");
 
     private final String filterName;
 
