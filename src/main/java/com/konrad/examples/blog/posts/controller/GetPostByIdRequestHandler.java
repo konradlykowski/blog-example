1fd9edb91929397a2e68f0e7d2647d12dfa36a29
Konrad Lykowski <konrad.lykowski@outlook.com>
2017-04-27 12:20:09 +0200

    add new func

--- src/main/java/com/konrad/examples/blog/posts/controller/GetPostByIdRequestHandler.java
+++ src/main/java/com/konrad/examples/blog/posts/controller/GetPostByIdRequestHandler.java
@@ -1,8 +1,9 @@
-package com.konrad.examples.blog.posts;
+package com.konrad.examples.blog.posts.controller;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.konrad.examples.blog.elasticsearch.ElasticSearchClient;
+import com.konrad.examples.blog.posts.input.validators.DefaultInputValidator;
 import com.konrad.examples.blog.posts.repository.PostsRepository;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -43,7 +44,7 @@ public class GetPostByIdRequestHandler implements RequestStreamHandler {
     }
 
     private String getPostId(JSONObject event) throws NullPointerException {
-        return ((JSONObject) event.get("pathParameters")).get("postId").toString();
+        return (new DefaultInputValidator()).validate(((JSONObject) event.get("pathParameters")).get("postId").toString());
     }
 
 
