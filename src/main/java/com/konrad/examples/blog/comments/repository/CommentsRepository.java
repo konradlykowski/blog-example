673480a7b233d79c96865cf8c48ab10dde80f1f6
Konrad Lykowski <konrad.lykowski@outlook.com>
2017-05-18 16:39:38 +0200

    add new func

--- src/main/java/com/konrad/examples/blog/comments/repository/CommentsRepository.java
+++ src/main/java/com/konrad/examples/blog/comments/repository/CommentsRepository.java
@@ -1,21 +1,25 @@
 package com.konrad.examples.blog.comments.repository;
 
+import com.konrad.examples.blog.comments.domain.Comment;
 import com.konrad.examples.blog.elasticsearch.ElasticSearchClient;
 import com.konrad.examples.blog.elasticsearch.query.ElasticSearchQueryBuilder;
 import org.springframework.web.client.RestTemplate;
 
 public class CommentsRepository {
-    private final RestTemplate restTemplate;
     ElasticSearchClient elasticSearchClient;
 
     public CommentsRepository(ElasticSearchClient elasticSearchClient, RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
         this.elasticSearchClient = elasticSearchClient;
+        this.elasticSearchClient.executeOn(restTemplate);
     }
 
     public String getCommentsByPostId(String postId) {
-        String[] searchBy = new String[]{postId};
+        String[] searchBy = new String[]{"postId"};
         return elasticSearchClient.executeQuery("/comments/_search", (new ElasticSearchQueryBuilder(0, searchBy, postId)).getQuery());
+    }
+
+    public String addComment(Comment comment) {
+        return elasticSearchClient.executeQuery("/comments/comment", (new ElasticSearchQueryBuilder(comment)).getCommentQuery());
     }
 
 }
