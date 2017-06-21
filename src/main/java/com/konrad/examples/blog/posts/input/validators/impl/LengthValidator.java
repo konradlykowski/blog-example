f94462a2b2a221e7216a8fc4ae56b028ec1afe7c
Konrad Lykowski <konrad.lykowski@outlook.com>
2017-06-21 16:42:31 +0200

    add new func

--- src/main/java/com/konrad/examples/blog/posts/input/validators/impl/LengthValidator.java
+++ src/main/java/com/konrad/examples/blog/posts/input/validators/impl/LengthValidator.java
@@ -3,9 +3,20 @@ package com.konrad.examples.blog.posts.input.validators.impl;
 import com.konrad.examples.blog.posts.input.validators.Validator;
 
 public class LengthValidator implements Validator<String> {
+
+    private final int len;
+
+    public LengthValidator() {
+        len = 30;
+    }
+
+    public LengthValidator(int len) {
+        this.len = len;
+    }
+
     @Override
     public String validate(String input) {
-        if (input.length() > 15) {
+        if (input.length() > len) {
             throw new IllegalArgumentException("LengthValidator");
         }
         return input;
