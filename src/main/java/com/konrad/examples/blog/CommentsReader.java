82917a6ab70cff9f94d98f0a6bed87efc97d93c8
Konrad Lykowski <konrad.lykowski@outlook.com>
2018-01-15 20:39:13 +0900

    add new func

---
+++ src/main/java/com/konrad/examples/blog/CommentsReader.java
@@ -0,0 +1,23 @@
+package com.konrad.examples.blog;
+
+import org.springframework.web.client.RestTemplate;
+
+public class CommentsReader {
+    private final String elasticSearchURL = "http://localhost:9200";
+    private final RestTemplate restTemplate;
+
+    public CommentsReader(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getComments(String postId) {
+        String query = "{\n" +
+                "    \"query\": {\n" +
+                "\"bool\": {\"should\": [{ \"match\": { \"postId\" : \"" + postId + "\"} }]}" +
+                "    },\n" +
+                "    \"sort\": { \"date\": { \"order\": \"desc\" }}\n" +
+                "} ";
+        return restTemplate.postForEntity(elasticSearchURL + "/comments/_search", query, String.class).getBody();
+    }
+
+}
